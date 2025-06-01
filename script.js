59de9f33119b574fb9f1857f7b2d4417f388fe8a
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-01 19:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+ const list=document.getElementById("book-list")
+        function addBook(event)
+        {
+            
+            event.preventDefault();
+            
+                if(title.value=="" || author.value=="" || isbn.value=="")
+                {
+                    alert("Enter all data");
+                    return;
+                }
+            list.innerHTML+=`<tr>
+                <td>${title.value}</td>
+                <td>${author.value}</td>
+                <td>${isbn.value}</td>
+                <td><button onclick="delBook(this)" class="delete">x</button></td>
+                </tr>`;
+                
+                title.value="";
+                author.value="";
+                isbn.value="";
+            
+        }
+        function delBook(button)
+        {
+           const del= button.parentElement.parentElement;
+           del.remove();
+        }
